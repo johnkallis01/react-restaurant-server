f08153ef2d55127e7de4835123b848ce4c65b1d2
John Kallis <johnkallis01@gmail.com>
2026-01-08 15:26:55 -0500

    comment out

--- api/auth/login.js
+++ api/auth/login.js
@@ -10,6 +10,7 @@ export default async function handler(req, res){
     // if (req.method === "OPTIONS") {
     //     return res.status(200).end();
     // }
+    console.log('server login')
     if (req.method !== "POST") return res.status(405).json({ message: "Method not allowed" });
     const {email, password} = req.body;
     try{
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 // import authVerify from './api/auth/verify.js';
-// import authLogin from './api/auth/login.js';
+import authLogin from './api/auth/login.js';
 import menuRoutes from './api/menus.js';
 // import cookieParser from "cookie-parser";
 const allowedOrigins = [
@@ -57,5 +57,5 @@ app.listen('5000', () => {
 app.use('/api/menus', menuRoutes);
 // app.use('/api/auth', authRoutes);
 // app.post('/api/auth/verify', authVerify);
-// app.post('/api/auth/login', authLogin);
+app.post('/api/auth/login', authLogin);
 export default app;
