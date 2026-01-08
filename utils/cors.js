df75f1471b4550273a1c56de3f46281584d57209
John Kallis <johnkallis01@gmail.com>
2026-01-08 16:02:04 -0500

    cors helper

---
+++ utils/cors.js
@@ -0,0 +1,19 @@
+const allowedOrigins = [
+  "https://react-restaurant-virid-nine.vercel.app",
+  "http://localhost:5173",
+];
+
+export function setCors(req, res) {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+}
