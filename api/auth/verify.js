6b7340d7cfe8ee54d9d061adbe0aec5a4d0a1bd8
John Kallis <johnkallis01@gmail.com>
2026-01-08 16:17:30 -0500

    delete auth.js

--- api/auth/verify.js
+++ api/auth/verify.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { setCors } from "../../utils/cors.js";
-
+import dotenv from 'dotenv';
 export default async function handler(req, res) {
     
   setCors(req, res);
