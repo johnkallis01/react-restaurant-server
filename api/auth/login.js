4452c1cde072bc0d352478d0939517fc74dd91b2
John Kallis <johnkallis01@gmail.com>
2026-01-08 15:29:48 -0500

    uncomment out

--- api/auth/login.js
+++ api/auth/login.js
@@ -3,13 +3,13 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.model.js';
 
 export default async function handler(req, res){
-    // res.setHeader("Access-Control-Allow-Origin", "https://react-restaurant-virid-nine.vercel.app");
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
-    // res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-    // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    // if (req.method === "OPTIONS") {
-    //     return res.status(200).end();
-    // }
+    res.setHeader("Access-Control-Allow-Origin", "https://react-restaurant-virid-nine.vercel.app");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
     console.log('server login')
     if (req.method !== "POST") return res.status(405).json({ message: "Method not allowed" });
     const {email, password} = req.body;
