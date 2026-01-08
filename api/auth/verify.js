4cc40865648dfa3411376e3bd24d93db7df379ff
John Kallis <johnkallis01@gmail.com>
2026-01-08 15:52:20 -0500

    added orogin

--- api/auth/verify.js
+++ api/auth/verify.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export default async function handler(req, res) {
+    
   res.setHeader("Access-Control-Allow-Origin", "https://react-restaurant-virid-nine.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
